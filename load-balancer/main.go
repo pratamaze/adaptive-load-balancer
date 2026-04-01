c1f4e872d12c7bde9a2bddd454972d534a9885a1
PratamarizkyPhe <pratamarizky249b@gmail.com>
2026-04-01 20:30:25 +0700

    fix(conection)max-conections-per-host

--- load-balancer/main.go
+++ load-balancer/main.go
@@ -248,7 +248,7 @@ func newReverseProxy(pool *NodePool) *httputil.ReverseProxy {
 	customTransport := &http.Transport{
 		MaxIdleConns:          10000, // Total koneksi keseluruhan
 		MaxIdleConnsPerHost:   5000,  // Batas koneksi nganggur per backend
-		MaxConnsPerHost:       0,     // Batas koneksi aktif per backend
+		MaxConnsPerHost:       15000, // Batas koneksi aktif per backend
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
